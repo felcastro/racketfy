e0f3cd4c2fa4665be22588b8f25735174d21d8d7
Felipe Castro <fpocastro@gmail.com>
2021-09-28 23:24:41 -0300

    Added Navbar component

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import {
+  Flex,
+  Heading,
+  FlexProps,
+  useBreakpointValue,
+  HStack,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import React from "react";
+import { ColorModeButton } from "../ColorModeButton";
+
+export const Navbar = (props: FlexProps) => {
+
+  return (
+    <Flex
+      as="header"
+      position="sticky"
+      zIndex="sticky"
+      top="0"
+      align="center"
+      justify="space-between"
+      h={14}
+      px={2}
+      boxShadow="md"
+      backgroundColor={useColorModeValue("white", "gray.800")}
+      {...props}
+    >
+      <Link href="/" passHref>
+        <Heading
+          as="a"
+          role="heading"
+          size={useBreakpointValue({ base: "md", sm: "lg" })}
+          fontWeight="extrabold"
+          mr={2}
+          color="pink.500"
+        >
+          racketfy
+        </Heading>
+      </Link>
+      <HStack align="center">
+        <ColorModeButton colorScheme="pink" />
+      </HStack>
+    </Flex>
+  );
+};
