82213be1191b2498db0e6f55deaf29412ec4cd53
Felipe Castro <fpocastro@gmail.com>
2021-10-04 21:54:53 -0300

    Downgraded yup resolver lib

--- pages/signin/index.tsx
+++ pages/signin/index.tsx
@@ -37,7 +37,6 @@ export default function SignIn() {
     formState: { errors, isSubmitting },
     watch,
   } = useForm<SignInFormParams>({
-    //@ts-ignore
     resolver: yupResolver(signInFormSchema),
   });
   const watchFields = watch();
