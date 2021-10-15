bcccd64c5871fdae299c290ef05b0ee0f19a1415
Felipe Castro <fpocastro@gmail.com>
2021-10-14 23:28:54 -0300

    Removed width limit on InfoCard

--- components/InfoCard/InfoCard.tsx
+++ components/InfoCard/InfoCard.tsx
@@ -2,7 +2,6 @@ import { Box, BoxProps, useColorModeValue } from "@chakra-ui/react";
 
 export const InfoCard = (props: BoxProps) => (
   <Box
-    maxW={["unset", "sm"]}
     bg={useColorModeValue("pink.100", "pink.900")}
     borderRadius="lg"
     boxShadow="md"
